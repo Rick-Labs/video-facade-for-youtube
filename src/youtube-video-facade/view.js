a606e2080e8c0ab07b91d6a743fa7e1d98887e3c
Lloyd Rick Guyon <78526686+ricklabsdotn8@users.noreply.github.com>
2025-05-20 04:35:01 +0800

    Add src folder and its contents to the repository

--- src/youtube-video-facade/view.js
+++ src/youtube-video-facade/view.js
@@ -1,18 +1,24 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const thumbnails = document.querySelectorAll('.youtube-thumbnail-preview');
-    thumbnails.forEach(thumbnail => {
-        thumbnail.addEventListener('click', function() {
-            const videoUrl = this.getAttribute('data-video-url');
-            if (videoUrl) {
-                const iframe = document.createElement('iframe');
-                iframe.setAttribute('width', '560');
-                iframe.setAttribute('height', '315');
-                iframe.setAttribute('src', videoUrl);
-                iframe.setAttribute('frameborder', '0');
-                iframe.setAttribute('allowfullscreen', '');
-                this.innerHTML = '';
-                this.appendChild(iframe);
+document.addEventListener("DOMContentLoaded", function () {
+    document.querySelectorAll(".play-button-overlay").forEach(function (button) {
+        button.addEventListener("click", function () {
+            const container = button.closest(".youtube-thumbnail-preview");
+            const videoId = getYouTubeId(container.getAttribute("data-video-url"));
+            if (videoId) {
+                const iframe = document.createElement("iframe");
+                iframe.setAttribute("width", "100%");
+                iframe.setAttribute("height", "100%");
+                iframe.setAttribute("src", `https://www.youtube.com/embed/${videoId}?autoplay=1`);
+                iframe.setAttribute("frameborder", "0");
+                iframe.setAttribute("allowfullscreen", "true");
+                container.innerHTML = "";
+                container.appendChild(iframe);
             }
         });
     });
+
+    function getYouTubeId(url) {
+        const regExp = /(?:youtube\.com\/(?:[^\/]+\/.+\/|(?:v|e(?:mbed)?)\/|.*[?&]v=)|youtu\.be\/)([a-zA-Z0-9_-]{11})/;
+        const match = url.match(regExp);
+        return match ? match[1] : null;
+    }
 });
