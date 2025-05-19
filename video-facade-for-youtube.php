d70a515f5bfbec6107f2d8a956bf8aab4eaa0a86
Lloyd Rick Guyon <78526686+ricklabsdotn8@users.noreply.github.com>
2025-05-20 03:51:18 +0800

    Revert "feat: make entire thumbnail clickable and update block registration"
    This reverts commit 0e067f9c477aaf58ae08e94c1b1d112da8dbbdf4.

--- video-facade-for-youtube.php
+++ video-facade-for-youtube.php
@@ -15,6 +15,18 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 function create_block_youtube_video_facade_block_init() {
-	register_block_type( __DIR__ . '/build/youtube-video-facade' );
+	if ( function_exists( 'wp_register_block_types_from_metadata_collection' ) ) {
+		wp_register_block_types_from_metadata_collection( __DIR__ . '/build', __DIR__ . '/build/blocks-manifest.php' );
+		return;
+	}
+
+	if ( function_exists( 'wp_register_block_metadata_collection' ) ) {
+		wp_register_block_metadata_collection( __DIR__ . '/build', __DIR__ . '/build/blocks-manifest.php' );
+	}
+
+	$manifest_data = require __DIR__ . '/build/blocks-manifest.php';
+	foreach ( array_keys( $manifest_data ) as $block_type ) {
+		register_block_type( __DIR__ . "/build/{$block_type}" );
+	}
 }
 add_action( 'init', 'create_block_youtube_video_facade_block_init' );
